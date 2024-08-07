bf113edb450901ac9f993a908cd5ba992e9aa02d
impacevanend <impacevanend@gmail.com>
2024-08-07 18:03:46 -0500

    se agrega estructura básica

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Blog</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Bienvenido a tu Blog</h1>
+    </div>
+</body>
+</html>
