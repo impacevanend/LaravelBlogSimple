10dc74cc9e79168cd0c427f92d08c14b05bbf953
impacevanend <impacevanend@gmail.com>
2024-08-07 18:27:02 -0500

    Se agrega controlador con métodos

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,10 +1,49 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        $post = Post::all();
+        return view('posts.index', compact('post'));
+    }
+
+    public function create(){
+        return view('posts.create');
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'title'=>'required',
+            'contect'=>'required',
+        ]);
+        Post::create($request->all());
+        return redirect()->route('posts.index')
+        ->with('success', 'Post creado exitosamente.');
+    }
+
+    public function show(Post $post){
+        return view('posts.show', compact('post'));
+    }
+
+    public function udpdate(Request $request, Post $post){
+        $request->validate([
+            'title'=>'required',
+            'content'=>'required',
+        ]);
+
+        $post->update($request->all());
+        return redirect()->route('posts.index')
+        -with('success','Post actualizado exitosamente.');
+    }
+
+    public function destroy(Post $post){
+        $post->delete();
+        return redirect()->route('posts.index')
+        ->with('success', 'Post eliminado exitosamente.');
+    }
+
 }
