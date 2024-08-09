0df18372ba5c524ce5796719d7beedd623c55b46
impacevanend <impacevanend@gmail.com>
2024-08-09 10:58:44 -0500

    se agregan vistas

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,14 +1,14 @@
 <?php
-
 namespace App\Http\Controllers;
+
 use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(){
-        $post = Post::all();
-        return view('posts.index', compact('post'));
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
     }
 
     public function create(){
@@ -18,18 +18,19 @@ class PostController extends Controller
     public function store(Request $request){
         $request->validate([
             'title'=>'required',
-            'contect'=>'required',
+            'content'=>'required',
         ]);
+
         Post::create($request->all());
         return redirect()->route('posts.index')
-        ->with('success', 'Post creado exitosamente.');
+            ->with('success', 'Post creado exitosamente.');
     }
 
     public function show(Post $post){
         return view('posts.show', compact('post'));
     }
 
-    public function udpdate(Request $request, Post $post){
+    public function update(Request $request, Post $post){
         $request->validate([
             'title'=>'required',
             'content'=>'required',
@@ -37,13 +38,12 @@ class PostController extends Controller
 
         $post->update($request->all());
         return redirect()->route('posts.index')
-        -with('success','Post actualizado exitosamente.');
+            ->with('success', 'Post actualizado exitosamente.');
     }
 
     public function destroy(Post $post){
         $post->delete();
         return redirect()->route('posts.index')
-        ->with('success', 'Post eliminado exitosamente.');
+            ->with('success', 'Post eliminado exitosamente.');
     }
-
 }
